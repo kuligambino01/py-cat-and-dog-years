68255f967f24170da388a0705bd365cd558c0cc8
kuligambino01 <czaro923@gmail.com>
2026-03-22 19:59:39 +0100

    'deletingwhitetrails'

--- app/main.py
+++ app/main.py
@@ -9,10 +9,10 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     Args:
         cat_age: Cat's age in cat years
         dog_age: Dog's age in dog years
-        
+
     Returns:
         List with [cat_human_age, dog_human_age]
-        
+
     Examples:
         get_human_age(0, 0) == [0, 0]
         get_human_age(15, 15) == [1, 1]
